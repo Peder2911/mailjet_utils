df41283c0b9912bbd36391a3f1fa884947dfc610
peder2911 <pglandsverk@gmail.com>
2022-02-22 16:20:39 +0100

    Fixed logging of posted data

--- mailjet_utils/mailjet_utils/app.py
+++ mailjet_utils/mailjet_utils/app.py
@@ -8,12 +8,13 @@ logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-@app.get("/{version}/send")
+@app.post("/{version}/send")
 def send_mail(send_data: models.SendData):
-    for message in send_data:
+    for message in send_data.Messages:
+        users = ", ".join([u.Email for u in message.To])
         logger.info((
-            f"Send email to {message.To} "
-            f"from {message.From}. "
+            f"Send email to {users} "
+            f"from {message.From.Email}. "
             f"\"{message.Subject}\" "
             f"({len(message.HTMLPart)} characters)"
             ))
